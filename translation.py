1a1678adcb1b57a79c03411372e429d4d48eedb3
MoulikMaity <104905898+MoulikMaity@users.noreply.github.com>
2022-05-11 13:10:44 +0530

    Create translation.py

---
+++ translation.py
@@ -0,0 +1,56 @@
+class LuciferMoringstar(object):
+
+    DEFAULT_MSG = """👋Hello {mention}.....!!!\nIt's Power Full [{bot_name}](t.me/{bot_username}) Here 😎\nAdd Me To Your Group And Make Sure I'm an Admin There! \nAnd Enjoy My Pever Show.....!!!🤪"""
+
+    HELP_MSG = """**Help Veno Monuse 🤣**"""
+
+    ABOUT_MSG = """
+🤖 My Name : [{bot_name}](t.me/{bot_username})
+
+🤴 My Dev : [DarkLord](t.me/hellodarklord)
+
+🧑‍💻 My Creator : {dev_name}
+
+🛠️ My Library : [Pyrogram]
+
+㊗️ My Language : [Python3]
+
+🗄️ My Database : [Mongodb]
+
+📡 My Server : [Heroku]"""
+
+    FILE_CAPTIONS = """Hello 👋 {mention}\n\n📁Title {title}\n\n🔘Size {size}"""
+
+    PR0FESS0R_99 = """
+**ADMINS COMMANDS**
+
+ » /broadcast - Reply Any Media Or Message
+ » /delete - Reply Files
+ » /deleteall - Delete All Files
+ » /total - How Many Files Saved
+ » /logger - Get Bot Logs
+ » /channel - Add Channel List"""
+
+    ADD_YOUR_GROUP = """**__ഞാൻ ജോലി ചെയ്യുന്ന ഗ്രൂപ്പിൽ ചേരൂ, നിങ്ങൾക്ക് ആവശ്യമുള്ള എല്ലാ സിനിമകളും ആ ഗ്രൂപ്പിൽ അഭ്യർത്ഥിക്കാം! ഇവിടെ അഭ്യർത്തിക്കരുത്..😤__**"""
+    SPELL_CHECK = """
+**Hello 👋{mention}**,
+
+**Couldn't Find {query}?  Please Click Your Request Movie Name**"""
+    GET_MOVIE_1 = """
+**Hello 👋{mention}
+
+
+↪️ **Requested Movie** : [{query}]({url})
+👤 **Requested By** : {mention}
+🎬 **Title** : [{title}]({url})
+📆 **Year** : {year}
+🌟 **Rating** {rating}/10
+🎭 **Genres** {genres}"""
+
+ © **{chat}**
+
+    GET_MOVIE_2 = """
+↪️ **Requested Movie** : {query}
+👤 **Requested By** : {mention}
+
+© **{chat}**"""
