a2747ebd75b55597ff4e1e33be9fe0586ab8191a
MoulikMaity <104905898+MoulikMaity@users.noreply.github.com>
2022-05-13 09:48:12 +0530

    Update callback.py

--- LuciferMoringstar_Robot/callback.py
+++ LuciferMoringstar_Robot/callback.py
@@ -299,7 +299,7 @@ async def cb_handler(client: lucifermoringstar_robot, query):
         
         elif query.data == "who":
             buttons = [[ 
-             InlineKeyboardButton("🏠 Home", callback_data="about"),       
+             InlineKeyboardButton("🏠 Home", callback_data="start"),       
              InlineKeyboardButton("Close 🗑️", callback_data="close")
              ]]
             await query.message.edit(text=LuciferMoringstar.WHO_AM_I.format(mention=query.from_user.mention, bot_name=bot_info.BOT_NAME, bot_username=bot_info.BOT_USERNAME, dev_name=DEV_NAME), reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
