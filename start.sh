425de2e329860cfb29d89e4e49718dec0acbd528
PROFESSOR-77 <109091765+PROFESSOR-77@users.noreply.github.com>
2022-07-16 14:17:53 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
-echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/DarkLord-99/LuciferMoringstar-Robot.git /LuciferMoringstar-Robot
+echo "Cloning Repo"
+git clone https://github.com/DarkLord-99/LuciferMoringstar-Robot.git /LuciferMoringstar-Robot
 cd /LuciferMoringstar-Robot
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
-echo "Starting Bot, Please Wait..."
+echo "Starting Bot..."
 python3 bot.py
 
