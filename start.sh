e38a4f91a8b46715b3721172609d39d5cbcb54ce
PROFESSOR-77 <109091765+PROFESSOR-77@users.noreply.github.com>
2022-07-16 14:22:04 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo"
-git clone https://github.com/DarkLord-99/LuciferMoringstar-Robot.git /LuciferMoringstar-Robot
+git clone https://github.com/PROFESSOR-77/LuciferMoringstar-Robot.git /LuciferMoringstar-Robot
 cd /LuciferMoringstar-Robot
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
